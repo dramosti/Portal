b11e6af25899f7dfcc82a088ab21d63a75a56eef
dramosti <diego@hlp.com.br>
2014-09-12 16:18:58 -0300

    a

--- HLP.Portal.MVC/Data/Repository/NoticiaRepository.cs
+++ HLP.Portal.MVC/Data/Repository/NoticiaRepository.cs
@@ -44,6 +44,40 @@ namespace HLP.Portal.MVC.Data.Repository
             }
         }
 
+        public List<NoticiaModel> GetNoticiaDestaque()
+        {
+            try
+            {
+                var Noticias = new List<NoticiaModel>();
+                ImagemRepository imagemRep = new ImagemRepository();
+                using (var db = new PortalEntities())
+                {
+                    var dados = db.tb_noticias.Where(c => c.stDestaque).Take(3).ToList();
+
+                    dados.ForEach((c) =>
+                    {
+                        var noticia = new NoticiaModel
+                        {
+                            idNoticias = c.idNoticias,
+                            dtNoticia = c.dtNoticia,
+                            stCarrocel = c.stCarrocel,
+                            stDestaque = c.stDestaque,
+                            xNoticia = c.xNoticia,
+                            xTitulo = c.xTitulo
+                        };
+                        noticia.Imagens = imagemRep.GetImagens(noticia.idNoticias, ImagemRepository.tabela.TB_NOTICIA);
+                        if (noticia.Imagens.Count() > 0)
+                            Noticias.Add(noticia);
+                    });
+                }
+                return Noticias;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
 
     }
 }
