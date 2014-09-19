f6c89f40e6eb1843d45f688ea4e4d42c08434626
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-09-19 13:37:11 -0300

    Sync

--- HLP.Portal.MVC/Areas/Solucoes/SolucoesAreaRegistration.cs
+++ HLP.Portal.MVC/Areas/Solucoes/SolucoesAreaRegistration.cs
@@ -1,22 +1,22 @@
 ﻿using System.Web.Mvc;
 
-namespace HLP.Portal.MVC.Areas.MagnificusEs
+namespace HLP.Portal.MVC.Areas.Solucoes
 {
-    public class MagnificusEsAreaRegistration : AreaRegistration
+    public class SolucoesAreaRegistration : AreaRegistration
     {
         public override string AreaName
         {
             get
             {
-                return "MagnificusEs";
+                return "Solucoes";
             }
         }
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
-                "MagnificusEs_default",
-                "MagnificusEs/{controller}/{action}/{id}",
+                "Solucoes_default",
+                "Solucoes/{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
         }
