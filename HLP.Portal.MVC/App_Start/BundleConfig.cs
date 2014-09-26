4174936ad6db483e18df98c57ac7a8399807435a
TakeeshiUtiyama <carlos.santos@hlp.com.br>
2014-09-26 11:15:27 -0300

    Sync

--- HLP.Portal.MVC/App_Start/BundleConfig.cs
+++ HLP.Portal.MVC/App_Start/BundleConfig.cs
@@ -17,13 +17,16 @@ namespace HLP.Portal.MVC.App_Start
                          "~/Scripts/bootbox.min.js",
                          "~/Scripts/jquery.maskedinput-{version}.js",
                          "~/Scripts/jquery.validate.min.js",
-                         "~/Scripts/jquery.validate.unobtrusive.min.js")
+                         "~/Scripts/jquery.validate.unobtrusive.min.js",
+                         "~/Scripts/jquery.jcarousel.min.js",
+                         "~/Scripts/jcarousel.responsive.js")
                          .IncludeDirectory("~/Scripts", ".js"));
 
             bundles.Add(new StyleBundle("~/bundles/css/desktop")
                 .Include("~/Content/bootstrap.min.css",
                           "~/Content/bootstrap-theme.min.css",
                           "~/Content/Site.css",
+                          "~/Content/carousel.css",
                           "~/Content/themes/pepper-grinder.css")
                           .IncludeDirectory("~/Content", ".css"));
         }
--- HLP.Portal.MVC/Controllers/HomeController.cs
+++ HLP.Portal.MVC/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using HLP.Portal.MVC.Data.Repository;
 using HLP.Portal.MVC.Models;
 using HLP.Portal.MVC.Models.Adm;
+using HLP.Portal.MVC.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace HLP.Portal.MVC.Controllers
 {
@@ -14,14 +16,26 @@ namespace HLP.Portal.MVC.Controllers
         //
         // GET: /Home/
 
+        public HomeController()
+        {
+            Log.xPath = @"C:\logPortal";
+        }
+
         NoticiaRepository noticiaRep = new NoticiaRepository();
 
         public ActionResult Home()
         {
             base.SessionHomeModel = new HomeModel();
-            base.SessionHomeModel.CarrosselNoticias = noticiaRep.GetNoticiaCarrossel();
-            base.SessionHomeModel.DestaqueNoticias = noticiaRep.GetNoticiaDestaque();
-
+            try
+            {
+                base.SessionHomeModel.CarrosselNoticias = noticiaRep.GetNoticiaCarrossel();
+                base.SessionHomeModel.DestaqueNoticias = noticiaRep.GetNoticiaDestaque();
+            }
+            catch (Exception ex)
+            {
+                Log.AddLog(xLog: ex.Message);
+                throw;
+            }
 
             return View(model: base.SessionHomeModel);
         }
